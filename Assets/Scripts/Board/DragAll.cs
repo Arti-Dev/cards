3177d4c438720b209f593e969912297f133911ea
Aidan Liu <boomcreeper296@gmail.com>
2025-08-11 16:13:42 -0400

    idk

---
+++ Assets/Scripts/Board/DragAll.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DragAll : MonoBehaviour
+{
+    private Transform dragging = null;
+    private Vector3 offset;
+    private InputAction leftClickAction;
+    private InputAction mousePositionAction;
+    [SerializeField] LayerMask dragMask;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        leftClickAction = InputSystem.actions.FindAction("LeftClick");
+        mousePositionAction = InputSystem.actions.FindAction("MousePosition");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // click
+        if (leftClickAction.IsPressed() && dragging == null)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(mousePositionAction.ReadValue<Vector2>()), Vector2.zero,
+        float.PositiveInfinity, dragMask);
+            HandleHit(hit);
+        }
+        // release
+        else if (!leftClickAction.IsPressed() && dragging != null)
+        {
+            Card card = dragging.GetComponent<Card>();
+            if (card != null) card.OnDrop();
+
+            dragging = null;
+        }
+
+        if (dragging != null)
+        {
+            dragging.position = Camera.main.ScreenToWorldPoint(mousePositionAction.ReadValue<Vector2>()) + offset;
+        }
+    }
+
+    void HandleHit(RaycastHit2D hit)
+    {
+        if (!hit) return;
+        Card card = hit.transform.GetComponent<Card>();
+        if (card == null) return;
+        if (!card.CanMove() || !card.GetPlayer().IsHuman()) return;
+        dragging = hit.transform;
+        offset = dragging.position - Camera.main.ScreenToWorldPoint(mousePositionAction.ReadValue<Vector2>());
+    }
+}
---
+++ Assets/Scripts/Board/PlayingArea.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayingArea : MonoBehaviour
+{
+    Player player;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayCard(Card card)
+    {
+        // todo check if there's already a card in play
+        MoveCardHere(card);
+        card.Show();
+        card.Play();
+        // todo if there are too many cards here some cards will become permanently unmovable
+    }
+
+    void MoveCardHere(Card card)
+    {
+        card.transform.SetParent(transform, true);
+        card.TransformLerp(transform.position);
+    }
+}
